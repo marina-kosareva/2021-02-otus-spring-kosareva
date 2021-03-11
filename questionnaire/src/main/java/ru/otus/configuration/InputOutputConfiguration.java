a96f43a8d432fc7fb8666d5884b3d528fdaaa1ef
marinakosareva <k_marina85@mail.ru>
2021-03-11 18:46:02 +0200

    HW2 refactoring

--- questionnaire/src/main/java/ru/otus/configuration/InputOutputConfiguration.java
+++ questionnaire/src/main/java/ru/otus/configuration/InputOutputConfiguration.java
@@ -1,20 +1,15 @@
 package ru.otus.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.otus.service.DefaultInputOutputService;
 import ru.otus.service.InputOutputService;
 
-import java.io.InputStream;
-import java.io.PrintStream;
-
 @Configuration
 public class InputOutputConfiguration {
 
     @Bean
-    InputOutputService defaultInputOutputService(@Value("#{ T(java.lang.System).in}") InputStream in,
-                                                 @Value("#{ T(java.lang.System).out}") PrintStream out) {
-        return new DefaultInputOutputService(in, out);
+    InputOutputService defaultInputOutputService() {
+        return new DefaultInputOutputService(System.in, System.out);
     }
 }
