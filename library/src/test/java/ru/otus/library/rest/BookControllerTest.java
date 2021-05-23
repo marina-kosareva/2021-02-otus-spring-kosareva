4c6641a1f17aa26747013ca84fe6fd987a705124
marinakosareva <k_marina85@mail.ru>
2021-05-23 14:59:06 +0300

    test refactoring

--- library/src/test/java/ru/otus/library/rest/BookControllerTest.java
+++ library/src/test/java/ru/otus/library/rest/BookControllerTest.java
@@ -8,20 +8,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.dao.OptimisticLockingFailureException;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.otus.library.dto.BookDto;
 import ru.otus.library.exceptions.BookRepositoryException;
-import ru.otus.library.mapper.BookMapperImpl;
 import ru.otus.library.request.CreateBookRequest;
 import ru.otus.library.request.UpdateBookRequest;
-import ru.otus.library.service.DefaultAuthorService;
-import ru.otus.library.service.DefaultBookService;
-import ru.otus.library.service.DefaultGenreService;
+import ru.otus.library.service.BookService;
 
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -29,18 +28,33 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = BookController.class)
-@Import({DefaultBookService.class, DefaultAuthorService.class, DefaultGenreService.class, BookMapperImpl.class})
 @AutoConfigureDataMongo
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class BookControllerTest {
 
     @Autowired
     private MockMvc mvc;
     @Autowired
     private ObjectMapper mapper;
+    @MockBean
+    private BookService service;
 
     @Test
     void getAllBooks() throws Exception {
+        when(service.getAll()).thenReturn(asList(BookDto.builder()
+                        .id("book1Id")
+                        .version(0L)
+                        .title("book1")
+                        .authorFullName("firstName1 lastName1")
+                        .genreTitle("genre1")
+                        .build(),
+                BookDto.builder()
+                        .id("book2Id")
+                        .version(0L)
+                        .title("book2")
+                        .authorFullName("firstName2 lastName2")
+                        .genreTitle("genre2")
+                        .build()));
+
         mvc.perform(get("/book"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(new JSONArray()
@@ -54,19 +68,22 @@ class BookControllerTest {
                                 .put("id", "book2Id")
                                 .put("version", 0L)
                                 .put("title", "book2")
-                                .put("genreTitle", "genre1")
-                                .put("authorFullName", "firstName2 lastName2"))
-                        .put(new JSONObject()
-                                .put("id", "book3Id")
-                                .put("version", 0L)
-                                .put("title", "book3")
                                 .put("genreTitle", "genre2")
                                 .put("authorFullName", "firstName2 lastName2")).toString()
                 ));
+
+        verify(service).getAll();
     }
 
     @Test
     void getById() throws Exception {
+        when(service.getBookDtoById("book3Id")).thenReturn(BookDto.builder()
+                .id("book3Id")
+                .version(0L)
+                .title("book3")
+                .authorFullName("firstName2 lastName2")
+                .genreTitle("genre2")
+                .build());
         mvc.perform(get("/book/book3Id"))
                 .andExpect(status().isOk())
                 .andExpect(content().json((new JSONObject()
@@ -77,25 +94,46 @@ class BookControllerTest {
                         .put("authorFullName", "firstName2 lastName2"))
                         .toString()
                 ));
+        verify(service).getBookDtoById("book3Id");
     }
 
     @Test
     void save() throws Exception {
+        when(service.create("title", "genre1Id", "author2Id"))
+                .thenReturn(BookDto.builder()
+                        .id("bookId")
+                        .version(0L)
+                        .title("title")
+                        .authorFullName("firstName2 lastName2")
+                        .genreTitle("genre1")
+                        .build());
+
         mvc.perform(post("/book")
                 .contentType(APPLICATION_JSON_VALUE)
                 .content(mapper.writeValueAsString(new CreateBookRequest("title", "genre1Id", "author2Id"))))
                 .andExpect(status().isCreated())
                 .andExpect(content().json(new JSONObject()
+                        .put("id", "bookId")
                         .put("version", 0L)
                         .put("title", "title")
                         .put("genreTitle", "genre1")
                         .put("authorFullName", "firstName2 lastName2")
                         .toString()
                 ));
+
+        verify(service).create("title", "genre1Id", "author2Id");
     }
 
     @Test
     void update() throws Exception {
+        when(service.update("book3Id", "titleNEW", 0L))
+                .thenReturn(BookDto.builder()
+                        .id("book3Id")
+                        .version(1L)
+                        .title("titleNEW")
+                        .authorFullName("firstName2 lastName2")
+                        .genreTitle("genre2")
+                        .build());
         mvc.perform(put("/book/book3Id")
                 .contentType(APPLICATION_JSON_VALUE)
                 .content(mapper.writeValueAsString(new UpdateBookRequest("titleNEW", 0L))))
@@ -108,20 +146,14 @@ class BookControllerTest {
                         .put("authorFullName", "firstName2 lastName2")
                         .toString()
                 ));
-    }
 
-    @Test
-    void update_optimisticException() throws Exception {
-        mvc.perform(put("/book/book3Id")
-                .contentType(APPLICATION_JSON_VALUE)
-                .content(mapper.writeValueAsString(new UpdateBookRequest("titleNEW", 1L))))
-                .andExpect(status().isBadRequest())
-                .andExpect(result -> assertTrue(result.getResolvedException()
-                        instanceof OptimisticLockingFailureException));
+        verify(service).update("book3Id", "titleNEW", 0L);
     }
 
     @Test
     void update_unknownId() throws Exception {
+        when(service.update("unknownBookId", "titleNEW", 1L))
+                .thenThrow(new BookRepositoryException("message"));
         mvc.perform(put("/book/unknownBookId")
                 .contentType(APPLICATION_JSON_VALUE)
                 .content(mapper.writeValueAsString(new UpdateBookRequest("titleNEW", 1L))))
@@ -134,5 +166,7 @@ class BookControllerTest {
     void deleteById() throws Exception {
         mvc.perform(delete("/book/book3Id"))
                 .andExpect(status().isNoContent());
+
+        verify(service).deleteById("book3Id");
     }
 }
