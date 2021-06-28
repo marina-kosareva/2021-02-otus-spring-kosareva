5288c3d37bb1cb89c79513ee3918e69a0949a58c
marinakosareva <k_marina85@mail.ru>
2021-06-28 16:45:32 +0300

    changes after code review

--- integration/src/main/java/ru/otus/integration/config/IntegrationConfig.java
+++ integration/src/main/java/ru/otus/integration/config/IntegrationConfig.java
@@ -1,5 +1,6 @@
 package ru.otus.integration.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.channel.PublishSubscribeChannel;
@@ -12,12 +13,15 @@ import org.springframework.integration.scheduling.PollerMetadata;
 import ru.otus.integration.domain.Appointment;
 import ru.otus.integration.domain.AppointmentRequest;
 import ru.otus.integration.domain.AppointmentResponse;
+import ru.otus.integration.service.AppointmentService;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Configuration
 public class IntegrationConfig {
+
+    private static final String GET_POSSIBLE_APPOINTMENTS_METHOD = "getPossibleAppointments";
 
     @Bean
     public QueueChannel appointmentRequestsChannel() {
@@ -35,27 +39,20 @@ public class IntegrationConfig {
     }
 
     @Bean
-    public IntegrationFlow appointmentFlow() {
+    public IntegrationFlow appointmentFlow(@Qualifier("hairAppointmentService") AppointmentService hairService,
+                                           @Qualifier("nailAppointmentService") AppointmentService nailService) {
         return IntegrationFlows.from("appointmentRequestsChannel")
                 .split()
                 .<AppointmentRequest, Boolean>route(AppointmentRequest::isNails,
                         m -> m
-                                .subFlowMapping(true, sf -> sf.gateway(appointmentNailsFlow()))
-                                .subFlowMapping(false, sf -> sf.gateway(appointmentHairFlow())))
+                                .subFlowMapping(true,
+                                        sf -> sf.gateway(f -> f.handle(nailService, GET_POSSIBLE_APPOINTMENTS_METHOD)))
+                                .subFlowMapping(false,
+                                        sf -> sf.gateway(f -> f.handle(hairService, GET_POSSIBLE_APPOINTMENTS_METHOD))))
                 .aggregate()
                 .transform(this::transformToAppointmentResponse)
                 .channel("appointmentResponseChannel")
                 .get();
-    }
-
-    @Bean
-    public IntegrationFlow appointmentHairFlow() {
-        return f -> f.handle("hairAppointmentService", "getPossibleAppointments");
-    }
-
-    @Bean
-    public IntegrationFlow appointmentNailsFlow() {
-        return f -> f.handle("nailAppointmentService", "getPossibleAppointments");
     }
 
     private AppointmentResponse transformToAppointmentResponse(List<List<Appointment>> appointmentDtoList) {
